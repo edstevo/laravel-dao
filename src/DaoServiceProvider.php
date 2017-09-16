5282124e7283e274cf476874f3cf078d68c7e4e7
edstevo <edward.stephenson@me.com>
2017-09-16 11:52:24 +0100

    service provider set up

--- src/DaoServiceProvider.php
+++ src/DaoServiceProvider.php
@@ -1,16 +1,20 @@
 <?php
 
-use EdStevo\Dao\Contracts\DaoBase;
-use EdStevo\Dao\Repositories\DaoBase as DaoBaseRepo;
-use EdStevo\Dao\Repositories\DaoCache;
-
 /**
  *  Copyright (c) 2017.
  *  This was created by Ed Stephenson (edward@flowflex.com).
  *  You must get permission to use this work.
  */
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\ServiceProvider;
+
 class DaoServiceProvider
 {
+    protected $contractPath = 'Contracts/Dao/';
+    protected $daoCachePath = 'Dao/Caches';
+    protected $daoModelPath = 'Dao/Models';
 
     /**
      * Bootstrap any application services.
@@ -29,6 +33,39 @@ class DaoServiceProvider
      */
     public function register()
     {
+        $this->getContracts()->each(function($contract) {
 
+            app()->bind('App\\Contracts\\Dao\\' . $contract, function($app) use ($contract) {
+
+                $daoRepository      = resolve('App\\Dao\\Models\\' . $contract);
+                $cacheRepository    = resolve('App\\Dao\\Caches\\' . $contract);
+
+                return new $cacheRepository($daoRepository);
+            });
+        });
+    }
+
+    /**
+     * Return all the Dao Contracts
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    private function getContracts() : Collection
+    {
+        return collect(Storage::disk('app')->allFiles($this->contractPath))->map(function($contract) {
+            return $this->getContractName($contract);
+        });
+    }
+
+    /**
+     * Return the name of the contract
+     *
+     * @param string $contract
+     *
+     * @return string
+     */
+    private function getContractName(string $contract)
+    {
+        return str_replace(".php", "", basename($contract));
     }
 }
